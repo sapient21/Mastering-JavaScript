379e3002ac410a0e6469da04a9088f10ea7964cb
Sapient <sapientrules@gmail.com>
2025-11-02 11:49:15 +0300

    . . . .

--- scripts/12.todo-list.js
+++ scripts/12.todo-list.js
@@ -11,10 +11,8 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = "";
 
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    //const {name} = todoObject.name;
-    //const dueDate = todoObject.dueDate;
+
+  todoList.forEach( function (todoObject,index) {
     const {name} = todoObject;
     const {dueDate} = todoObject;
 
@@ -22,13 +20,14 @@ function renderTodoList() {
     <div>${name}</div>
     <div>${dueDate}</div> 
     <button onclick = "
-      todoList.splice(${i}, 1);
+      todoList.splice(${index}, 1);
       renderTodoList();
     " class= "delete-todo-btn">delete</button>
     `;
 
     todoListHTML += html;
-  }
+  })
+  
 
   //console.log(todoListHTML);
 
