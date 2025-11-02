2f8e47c89fa15f1466d3c9ab0d156b7eb4e3c554
Sapient <sapientrules@gmail.com>
2025-11-02 13:48:51 +0300

    . . .

--- scripts/12.todo-list.js
+++ scripts/12.todo-list.js
@@ -1,9 +1,12 @@
-const todoList = [ {
-  name: "watch",
-dueDate: '2025-11-1'},
+const todoList = [
   {
-  name: "learn",
-  dueDate: '2025-11-2'},
+    name: "watch",
+    dueDate: "2025-11-1",
+  },
+  {
+    name: "learn",
+    dueDate: "2025-11-2",
+  },
 ];
 
 renderTodoList();
@@ -11,10 +14,9 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = "";
 
-
-  todoList.forEach((todoObject,index) => {
-    const {name} = todoObject;
-    const {dueDate} = todoObject;
+  todoList.forEach((todoObject, index) => {
+    const { name } = todoObject;
+    const { dueDate } = todoObject;
 
     const html = `
     <div>${name}</div>
@@ -22,31 +24,42 @@ function renderTodoList() {
     <button onclick = "
       todoList.splice(${index}, 1);
       renderTodoList();
-    " class= "delete-todo-btn">delete</button>
+    " class= "delete-todo-btn js-delete-todo-btn">delete</button>
     `;
 
     todoListHTML += html;
-  })
-  
+  });
 
   //console.log(todoListHTML);
 
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
-};
+
+  document.querySelectorAll(".js-delete-todo-btn")
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
+}
+
+document.querySelector(".js-add-todo-btn").addEventListener("click", () => {
+  addTodo();
+});
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
 
-const dateInputElement = document.querySelector('.js-due-date-input');
+  const dateInputElement = document.querySelector(".js-due-date-input");
 
-const dueDate = dateInputElement.value;
+  const dueDate = dateInputElement.value;
 
   todoList.push({
     //name: name,
     //dueDate: dueDate
     name,
-    dueDate
+    dueDate,
   });
 
   //console.log(todoList);
